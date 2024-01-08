b220fac44d33bd2484c4521bc5c9d3a6dc9a899d
Dirk Heilig <heilig@adesso-mobile.de>
2024-01-08 20:19:45 +0100

    change layout

---
+++ themes/hugo-theme-flat/static/js/layout.js
@@ -0,0 +1,10 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+document.querySelectorAll('a[href^="http"]').forEach(function(element) {
+    const myhost = window.location.hostname;
+    const linkhost=new URL(element.href).hostname;
+    if(myhost === linkhost) return;
+    element.setAttribute("target","_blank");
+console.log(element.attributes)
+})
+});
