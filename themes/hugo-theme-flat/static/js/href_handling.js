704c9c0cbf4735aa354c4b1d42b3afbaf7dd98a1
Dirk Heilig <heilig@adesso-mobile.de>
2024-01-09 13:05:30 +0100

    Flutizität erhöhen (link prefetch=

---
+++ themes/hugo-theme-flat/static/js/href_handling.js
@@ -0,0 +1,18 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+document.querySelectorAll('a').forEach(function(element) {
+    const myhost = window.location.hostname;
+    const linkhost=new URL(element.href).hostname;
+    if(myhost === linkhost) {
+        let prefetchUrl=new URL(element.href).toString()
+        // lest make a prefetch tag for this url
+        let link = document.createElement('link');
+        link.setAttribute('rel', 'prefetch');
+        link.setAttribute('href', prefetchUrl);
+        link.setAttribute('as', 'document');
+        document.head.appendChild(link);
+    }else {
+        element.setAttribute("target", "_blank");
+    }
+})
+});
--- themes/hugo-theme-flat/static/js/layout.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function() {
-document.querySelectorAll('a[href^="http"]').forEach(function(element) {
-    const myhost = window.location.hostname;
-    const linkhost=new URL(element.href).hostname;
-    if(myhost === linkhost) return;
-    element.setAttribute("target","_blank");
-})
-});
